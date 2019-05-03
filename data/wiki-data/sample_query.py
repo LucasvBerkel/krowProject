02a4fb89973cb5d5f87b097e050e06e37be05ea1
Lucas van Berkel <lucasvberkel@gmail.com>
2019-05-03 15:25:23 +0200

    Datadumps to be used

---
+++ data/wiki-data/sample_query.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+url = 'https://query.wikidata.org/sparql'
+query = """
+PREFIX wd: <http://www.wikidata.org/entity/> 
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+
+SELECT ?q WHERE {
+  ?q wdt:P31/wdt:P279* wd:Q11424
+}
+"""
+r = requests.get(url, params = {'format': 'json', 'query': query})
+data = r.json()
+
+with open('sample_query.json', 'w') as outfile:
+    json.dump(data, outfile)
